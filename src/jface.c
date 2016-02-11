dcdf79c641eec9effe23afd3af8547ad0cbd0a1b
j2ko <j2ko.pro@gmail.com>
2016-02-11 04:48:54 +0200

    added support of settings screen. reduced line width to 80 symbols. small refactoring

--- src/jface.c
+++ src/jface.c
@@ -1,16 +1,23 @@
 #include <pebble.h>
 
 #ifndef DEBUG
-#define TIME_FMT_STR "%H:%M" 
+#define TIME_FMT_24_STR "%I:%M" 
+#define TIME_HANDLER_UNIT MINUTE_UNIT
 #define DLOG(MSG)
 #define TRACE()
 #else
-#define TIME_FMT_STR "%M:%S"
+#define TIME_FMT_24_STR "%M:%S"
+#define TIME_HANDLER_UNIT SECOND_UNIT
 #define DLOG(MSG) printf(MSG)
 #define TRACE() printf("%s\n",__FUNCTION__)
 #endif
+#define TIME_FMT_12_STR "%I:%M"
+#define KEY_BACKGROUND_COLOR 0
+#define KEY_TIME_COLOR 1
+#define KEY_DATE_COLOR 2
+#define KEY_TWENTY_FOUR_HOUR_FORMAT 3
+#define KEY_DEFAULT_CHANGED 4
 
-static const char * TIME_FMT = TIME_FMT_STR;
 static const char * DATE_FMT = "%a %d  | %b";
 
 //Watchface ctx, holds reference to all resources
@@ -26,6 +33,12 @@ typedef struct {
     Layer *canvas_layer;
     GBitmap *bitmap;
     bool is_connected;
+    struct {
+       uint32_t backgroundColor;
+       uint32_t timeColor;
+       uint32_t dateColor;
+       bool is_time_format_24;
+    } settings;
 } krang_ctx_t;
 
 //For now using this function as context holder/getter.
@@ -35,15 +48,50 @@ krang_ctx_t* get_context() {
     return &krang_ctx;
 }
 
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+static krang_ctx_t* load_persistent_data(krang_ctx_t* ctx) {
+   TRACE();
+   if (persist_read_bool(KEY_DEFAULT_CHANGED)) {
+       ctx->settings.backgroundColor = persist_read_int(KEY_BACKGROUND_COLOR);
+       ctx->settings.timeColor = persist_read_int(KEY_TIME_COLOR);           
+       ctx->settings.dateColor = persist_read_int(KEY_DATE_COLOR);           
+       ctx->settings.is_time_format_24 = 
+           persist_read_bool(KEY_TWENTY_FOUR_HOUR_FORMAT);
+   } else {
+       //Default config (fresh install)
+       ctx->settings.backgroundColor = 0xFFAA00;// YellowChrome
+       ctx->settings.timeColor = 0x5500AA; // Indigo        
+       ctx->settings.dateColor = 0xFFFFFF; // White 
+       ctx->settings.is_time_format_24 = true; 
+   }
+   
+   return ctx;
+}
+
+const char* get_timeformat(bool is_24_hour) {
+   return is_24_hour ? TIME_FMT_24_STR : TIME_FMT_12_STR; 
+}
+
+static void update_time(struct tm *tick_time) {
     TRACE();
+    if (tick_time == NULL) {
+        time_t now = time(NULL);
+        tick_time = localtime(&now);
+    }
+    
     static char time_buff[6];
-    strftime(time_buff, sizeof(time_buff), TIME_FMT, tick_time);
+    strftime(time_buff, sizeof(time_buff), 
+            get_timeformat(get_context()->settings.is_time_format_24), 
+            tick_time);
     text_layer_set_text(get_context()->time_layer, time_buff);
 
     static char date_buff[32];
     strftime(date_buff, sizeof(date_buff), DATE_FMT, tick_time);
     text_layer_set_text(get_context()->date_layer, date_buff);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+    TRACE();
+    update_time(tick_time);
 }
 
 static void battery_handler(BatteryChargeState new_state) {
@@ -56,6 +104,52 @@ static void connection_handler(bool is_connected) {
     TRACE();
     get_context()->is_connected = is_connected;
     layer_mark_dirty(get_context()->canvas_layer);
+    
+}
+
+static void invalidate() { 
+    TRACE();
+    krang_ctx_t *ctx = get_context();
+    window_set_background_color(ctx->main_window, GColorFromHEX(ctx->settings.backgroundColor) /*GColorChromeYellow*/ );
+    text_layer_set_text_color(ctx->time_layer, GColorFromHEX(ctx->settings.timeColor));
+    text_layer_set_text_color(ctx->date_layer, GColorFromHEX(ctx->settings.dateColor));
+    update_time(NULL);
+    layer_mark_dirty(get_context()->canvas_layer);  
+    //todo: invalidate should also refresh all visible items - time/date
+}
+
+static void inbox_received_handler(DictionaryIterator *iter, void *context) {
+    TRACE();
+    Tuple *background_color_t = dict_find(iter, KEY_BACKGROUND_COLOR);
+    Tuple *time_color_t = dict_find(iter, KEY_TIME_COLOR);
+    Tuple *date_color_t = dict_find(iter, KEY_DATE_COLOR);
+    Tuple *twenty_four_hour_format_t = dict_find(iter, KEY_TWENTY_FOUR_HOUR_FORMAT);
+
+    if (background_color_t) {
+        int background_color = background_color_t->value->int32;
+
+        persist_write_int(KEY_BACKGROUND_COLOR, background_color);
+    }
+
+    if (time_color_t) {
+        int time_color = time_color_t->value->int32;
+        persist_write_int(KEY_TIME_COLOR, time_color);
+    }
+
+    if (date_color_t) {
+        int date_color = date_color_t->value->int32;
+        persist_write_int(KEY_DATE_COLOR, date_color);
+    }  
+
+    if (twenty_four_hour_format_t) {
+        bool twenty_four_hour_format = twenty_four_hour_format_t->value->int8;
+
+        persist_write_int(KEY_TWENTY_FOUR_HOUR_FORMAT, twenty_four_hour_format);
+    }
+    
+    persist_write_bool(KEY_DEFAULT_CHANGED, true);
+    load_persistent_data(get_context());
+    invalidate();
 }
 
 static void main_update_proc(Layer *layer, GContext *gctx) {  
@@ -65,8 +159,14 @@ static void main_update_proc(Layer *layer, GContext *gctx) {
 
     const int margin = 6;
     const int height = 60;
-    GRect border = GRect(margin, canvas_bounds.size.h - height - margin, canvas_bounds.size.w - 2*margin, height);
-    GRect inner  = GRect(border.origin.x + 2, border.origin.y + 2, border.size.w - 4, border.size.h - 4);
+    GRect border = GRect(margin, 
+            canvas_bounds.size.h - height - margin, 
+            canvas_bounds.size.w - 2*margin, 
+            height);
+    GRect inner  = GRect(border.origin.x + 2, 
+            border.origin.y + 2, 
+            border.size.w - 4, 
+            border.size.h - 4);
 
     //Inner border
     graphics_context_set_stroke_width(gctx, 4);
@@ -79,13 +179,17 @@ static void main_update_proc(Layer *layer, GContext *gctx) {
     graphics_draw_round_rect(gctx, border , 15);
 
     //Date backround
-    GRect date_fix = GRect(margin*2, canvas_bounds.size.h/2 + 23, canvas_bounds.size.w - margin*4, 14);
-    graphics_context_set_fill_color(gctx, GColorIndigo);
+    GRect date_fix = GRect(margin*2, 
+            canvas_bounds.size.h/2 + 23, 
+            canvas_bounds.size.w - margin*4, 
+            14);
+    graphics_context_set_fill_color(gctx, GColorFromHEX(ctx->settings.timeColor));
     graphics_context_set_stroke_color(gctx, GColorIndigo);
     graphics_fill_rect(gctx, date_fix, 0, GCornerNone);
 
     //Battery indicator
-    int charge_percent = ctx->battery_state.is_charging ? 100 : ctx->battery_state.charge_percent;
+    int charge_percent = ctx->battery_state.is_charging ? 
+        100 : ctx->battery_state.charge_percent;
 
     GRect indicator_border = GRect(margin, margin, 15, 11);    
     GRect battery_level = GRect(margin, margin, (15*charge_percent)/100, 10);
@@ -97,13 +201,16 @@ static void main_update_proc(Layer *layer, GContext *gctx) {
 
     //Battery charging        
     if (ctx->battery_state.is_charging) {
-        GPoint from = GPoint(indicator_border.origin.x + 3, indicator_border.origin.y + 5);
+        GPoint from = GPoint(indicator_border.origin.x + 3, 
+                indicator_border.origin.y + 5);
         GPoint to = GPoint(from.x + 3, from.y);
         graphics_context_set_stroke_color(gctx, GColorWhite);
         graphics_draw_line(gctx, from, to);
         graphics_draw_rect(gctx, GRect(to.x, to.y-2, 3, 5));
-        graphics_draw_line(gctx, GPoint(to.x + 3, to.y - 1), GPoint(to.x + 4, to.y - 1));
-        graphics_draw_line(gctx, GPoint(to.x + 3, to.y + 1), GPoint(to.x + 4, to.y + 1));
+        graphics_draw_line(gctx, GPoint(to.x + 3, to.y - 1), 
+                GPoint(to.x + 4, to.y - 1));
+        graphics_draw_line(gctx, GPoint(to.x + 3, to.y + 1), 
+                GPoint(to.x + 4, to.y + 1));
     }
 
     //Bluetooth connection indicator   
@@ -112,7 +219,8 @@ static void main_update_proc(Layer *layer, GContext *gctx) {
         //Bluetooth connection indicator vertical
         int width = 18;
         int height = 10;        
-        GPoint tmp, pivot = GPoint(canvas_bounds.size.w - margin - width, margin + 6);        
+        GPoint tmp, pivot = GPoint(canvas_bounds.size.w - margin - width,
+                margin + 6);        
         graphics_context_set_stroke_color(gctx, GColorIndigo);
 
         graphics_draw_line(gctx, pivot, GPoint(pivot.x + width, pivot.y));
@@ -129,7 +237,10 @@ static void main_update_proc(Layer *layer, GContext *gctx) {
 
 static void main_window_load(Window *window) {
     TRACE();
-    krang_ctx_t *ctx = get_context();
+    krang_ctx_t *ctx = load_persistent_data(get_context());
+    
+    window_set_background_color(ctx->main_window, GColorFromHEX(ctx->settings.backgroundColor) /*GColorChromeYellow*/ );
+
 
     // Get information about the Window
     Layer *window_layer = window_get_root_layer(window);
@@ -147,31 +258,38 @@ static void main_window_load(Window *window) {
     bitmap_layer_set_compositing_mode(ctx->bitmap_layer, GCompOpSet);
     const int heightTime = 42;
     const int offsetTime = 15;
-    ctx->time_layer = text_layer_create(GRect(0, canvas_bounds.size.h - heightTime - offsetTime , canvas_bounds.size.w, heightTime));
-    text_layer_set_text_color(ctx->time_layer, GColorIndigo);
+    ctx->time_layer = text_layer_create(GRect(0, 
+                canvas_bounds.size.h - heightTime - offsetTime , 
+                canvas_bounds.size.w, heightTime)
+            );
+    text_layer_set_text_color(ctx->time_layer, GColorFromHEX(ctx->settings.timeColor) /*GColorIndigo*/);
     text_layer_set_background_color(ctx->time_layer, GColorClear);
-    text_layer_set_font(ctx->time_layer, fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS));
+    text_layer_set_font(ctx->time_layer, 
+            fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS)
+            );
     text_layer_set_text_alignment(ctx->time_layer, GTextAlignmentCenter);
 
     //todo: I need custom font for date - to much extra space on top of default
     const int heightDate = 16;
     const int offsetDate = 20;
-    ctx->date_layer = text_layer_create(GRect(0, canvas_bounds.size.h/2 + offsetDate, canvas_bounds.size.w, heightDate));
-    text_layer_set_text_color(ctx->date_layer, GColorWhite);
+    ctx->date_layer = text_layer_create(GRect(0, 
+                canvas_bounds.size.h/2 + offsetDate, 
+                canvas_bounds.size.w, heightDate)
+            );
+    text_layer_set_text_color(ctx->date_layer, GColorFromHEX(ctx->settings.dateColor));
     text_layer_set_background_color(ctx->date_layer, GColorClear);
-    text_layer_set_font(ctx->date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+    text_layer_set_font(ctx->date_layer, 
+            fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
     text_layer_set_text_alignment(ctx->date_layer, GTextAlignmentCenter);
 
-    // Add it as a child layer to the Window's root layer
+    //Add it as a child layer to the Window's root layer
     layer_add_child(window_layer, ctx->canvas_layer);
     layer_add_child(window_layer, text_layer_get_layer(ctx->time_layer));
     layer_add_child(window_layer, text_layer_get_layer(ctx->date_layer));
     layer_add_child(window_layer, bitmap_layer_get_layer(ctx->bitmap_layer));
-
+   
     //Initial fill of time/date values
-    time_t now = time(NULL);
-    struct tm *t = localtime(&now);
-    tick_handler(t, 0);
+    update_time(NULL);
 
     //Initial values of battery level/connection state
     ctx->battery_state = battery_state_service_peek();
@@ -181,6 +299,7 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
     TRACE();
     krang_ctx_t *ctx = get_context();
+    
     bitmap_layer_destroy(ctx->bitmap_layer);
     gbitmap_destroy(ctx->bitmap);
     text_layer_destroy(ctx->time_layer);
@@ -192,21 +311,22 @@ void handle_init(void) {
     TRACE();
     Window* main_window = window_create();
     get_context()->main_window = main_window;
-
-    window_set_background_color(main_window,GColorChromeYellow);
-
+    
     window_set_window_handlers(main_window, (WindowHandlers) {
             .load = main_window_load,
             .unload = main_window_unload
     }); 
 
-    tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+    tick_timer_service_subscribe(TIME_HANDLER_UNIT, tick_handler);
     window_stack_push(main_window, true);
 
     battery_state_service_subscribe(battery_handler);  
     connection_service_subscribe((ConnectionHandlers) {
             .pebblekit_connection_handler = connection_handler,
     });
+    app_message_register_inbox_received(inbox_received_handler);
+    app_message_open(app_message_inbox_size_maximum(), 
+            app_message_outbox_size_maximum());
 }
 
 void handle_deinit(void) {
---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,30 @@
+Pebble.addEventListener('ready', function() {
+  console.log('PebbleKit JS ready!');
+});
+
+Pebble.addEventListener('showConfiguration', function() {
+  var url = 'TBD';
+  
+  console.log('Showing configuration page: ' + url);
+
+  Pebble.openURL(url);
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  var configData = JSON.parse(decodeURIComponent(e.response));
+
+  console.log('Configuration page returned: ' + JSON.stringify(configData));
+
+  if (configData.backgroundColor) {
+    Pebble.sendAppMessage({
+      backgroundColor: parseInt(configData.backgroundColor, 16),
+      timeColor: parseInt(configData.timeColor, 16),
+      dateColor: parseInt(configData.dateColor, 16),
+      twentyFourHourFormat: configData.twentyFourHourFormat
+    }, function() {
+      console.log('Send successful!');
+    }, function() {
+      console.log('Send failed!');
+    });
+  }
+});
